d030c7ca89ec8bebdf46af0507bf4dec6b8be42f
kbnez <koyeb@o2sky.nyc.mn>
2025-06-16 22:03:41 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "o2sky"         # 用户名
+UUID = "ac27dfbb-3981-455e-ab73-d74d20d73082"                     # UUID，留空则自动生成
+PORT = 8001                   # Vmess端口，留空或0则自动生成
+DOMAIN = "streamlitjp.897.pp.ua"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZTk1NzI5NzVkZTg4MGJjYTQxODk2ODk2NmJlZTJiN2MiLCJ0IjoiZmUxNzhmNWQtMmFjYy00ZDE0LWI2NDQtYmMzMDcyNjUzMDdhIiwicyI6Ik5XWXhaamRsWVRRdE1EZGpNaTAwWWpBekxUZzJNbU10T0RNNU1qTmxORE0yTVRjeCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
